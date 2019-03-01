bb243b97573153481c54f342291586889d6daa4c
vvv1 <havocf4u@gmail.com>
2019-03-01 10:04:18 +0100

    Designing urls in process

--- mydrugstore/admin.py
+++ mydrugstore/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from mydrugstore import models
 
 # Register your models here.
+
+admin.site.register(models.CarritoDeCompra)
+admin.site.register(models.Cataleg)
+admin.site.register(models.Estoc)
+admin.site.register(models.Producte)
+admin.site.register(models.Proveedor)
+admin.site.register(models.Usuari)
+
+
+
