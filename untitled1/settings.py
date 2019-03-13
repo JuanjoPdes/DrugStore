a26037748b927c260f36e996b6561ed82c80288f
EvalRaman <havocf4u@gmail.com>
2019-03-13 10:33:58 +0100

    First template done

--- untitled1/settings.py
+++ untitled1/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS':  [os.path.join(BASE_DIR, 'templates'),
-        os.path.join(os.path.dirname(__file__), '../templates')]
+        os.path.join(os.path.dirname(__file__), '../templates'),'Templates/']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- untitled1/urls.py
+++ untitled1/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from mydrugstore import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('mydrugstore/', include(('mydrugstore.urls', 'mydrugstore'), namespace = 'mydrugstore')),
+    path(r'^admin/', admin.site.urls),
+    url(r'firsttemplate/', views.index),
 ]
